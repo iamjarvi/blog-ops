876c75545a3f89ab98f692ee20199e59a21ce743
iamjarvi <anthony.m.jarvis@gmail.com>
2021-09-20 13:34:09 -0500

    Fixed broken route in Welcome Screen.

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -6,7 +6,11 @@
         <h2 v-else>{{ post.blogTitle }}</h2>
         <p v-if="post.welcomeScreen">{{ post.blogPost }}</p>
         <p class="content-preview" v-else v-html="post.blogHTML"></p>
-        <router-link class="link link-light" v-if="post.welcomeScreen" to="#">
+        <router-link
+          class="link link-light"
+          v-if="post.welcomeScreen"
+          :to="{ name: 'Login' }"
+        >
           Login/Register<Arrow class="arrow arrow-light" />
         </router-link>
         <router-link
